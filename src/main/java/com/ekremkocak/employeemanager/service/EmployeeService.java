426cb78706536056e5e4112f4316190a31abb69a
ekrem-kocak <ekrem-kocak@outlook.com>
2024-03-10 17:16:17 +0300

    Employee Service and Exception Handle

--- src/main/java/com/ekremkocak/employeemanager/service/EmployeeService.java
+++ src/main/java/com/ekremkocak/employeemanager/service/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.ekremkocak.employeemanager.service;
 
+import com.ekremkocak.employeemanager.exception.UserNotFoundException;
 import com.ekremkocak.employeemanager.model.Employee;
 import com.ekremkocak.employeemanager.repo.EmployeeRepo;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +22,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found!"));
+
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
     }
 }
